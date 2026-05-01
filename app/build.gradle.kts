ab80d6e573d5e5d2f5ec119a897e8532c92600fa
Hector Manrique <hmanrique32@gmail.com>
2026-05-01 22:05:36 +0200

    Upload new version to firebase app distribution - 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.jstr14.picaday"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
